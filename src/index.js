df7d958352d9762edeb68df3b76ade17a09e7195
Mukhtar Bahadory <mukhtar5.bahadory5@gmail.com>
2021-05-20 14:07:45 -0500

    init

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react'
-import  {store, persistor}  from './redux/store.js';
+import { store, persistor } from './redux/store.js';
 
 import './index.css';
 import App from './App';
